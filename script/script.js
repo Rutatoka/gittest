389581b8aaeae5f60d69cabdb8ca1c5dd7ce3309
Angelina <lunina.angelinka2002@gmail.com>
2021-05-30 23:42:37 +0200

    main

--- script/script.js
+++ script/script.js
@@ -3,12 +3,14 @@ let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 let x = 10,
     y = 10;
+let gameEngine;
 
 function drawRect() {
     context.fillStyle = "black";
     context.clearRect(0, 0, 300, 300)
     context.fillRect(x, y, 50, 50);
 }
+
 let nexGameStep = (function() {
     return requestAnimationFrame ||
         webkitRequestAnimationFrame ||
@@ -19,9 +21,33 @@ let nexGameStep = (function() {
             window.setTimeout(callback, 1000 / 60);
         };
 })();
-let gameLoop = function() {
+
+let gameEngineStart = function(callback) {
+    gameEngine = callback;
+    gameEngineStep();
+}
+let gameEngineStep = function() {
+    gameEngine();
+    nexGameStep(gameEngineStep);
+};
+let setGameEngine = function(callback) {
+    gameEngine = callback;
+}
+let gameLoopRight = function() {
     drawRect();
     x += 1;
-    nexGameStep(gameLoop);
+    if (x >= 250) {
+        setGameEngine(gameLoopLeft);
+    }
 };
-gameLoop();
+let gameLoopLeft = function() {
+    drawRect();
+    x -= 1;
+
+    if (x < 0) {
+        setGameEngine(gameLoopRight);
+    }
+};
+
+
+gameEngineStart(gameLoopRight)
