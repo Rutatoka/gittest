13bd3470633d15b1ac83ab6faf9ca1d6af6b2fe4
Angelina <lunina.angelinka2002@gmail.com>
2021-05-31 22:35:19 +0200

    main

--- script/script.js
+++ script/script.js
@@ -1,10 +1,11 @@
 "use strict"
+// создаем 2д пространоство
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 let x = 10,
     y = 10;
 let gameEngine;
-
+// рисуем квадрат
 function drawRect() {
     context.fillStyle = "black";
     context.clearRect(0, 0, 300, 300)
@@ -31,8 +32,9 @@ let gameEngineStep = function() {
     nexGameStep(gameEngineStep);
 };
 let setGameEngine = function(callback) {
-    gameEngine = callback;
-}
+        gameEngine = callback;
+    }
+    // едет вправо
 let gameLoopRight = function() {
     drawRect();
     x += 5;
@@ -40,14 +42,48 @@ let gameLoopRight = function() {
         setGameEngine(gameLoopLeft);
     }
 };
+// едет влево
 let gameLoopLeft = function() {
     drawRect();
     x -= 5;
-
     if (x < 0) {
         setGameEngine(gameLoopRight);
     }
 };
 
-
 gameEngineStart(gameLoopRight)
+
+// загрузка аудио
+function loadAudio(arr) {
+    let audio = document.createElement("audio")
+    for (let i = 0, len = arr.length; i < len; i += 1) {
+        let source = document.createElement("source")
+        source.src = arr[i];
+        audio.appendChild(source);
+    }
+
+    let o = {
+        dom: false,
+        state: "stop",
+        play: function() {
+            this.dom.play();
+            this.state = "play";
+        },
+        pause: function() {
+            this.dom.pause();
+            this.state = "pause";
+        },
+        stop: function() {
+            this.dom.pause();
+            this.dom.currentTime = 0;
+            this.state = "stop";
+        }
+
+
+    };
+    o.dom = audio
+    return o;
+}
+let a1 = loadAudio(['jumpo.wav']);
+let a2 = loadAudio(['theme.mp3']);
+a1.play();
